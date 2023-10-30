cca4532df39258bdfe40ca6a9307a2a6682f6d58
Noosly <139576679+Noosly@users.noreply.github.com>
2023-10-30 01:42:19 +0100

    Faire les Routes vers les pages

---
+++ src/pages/Apropos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Apropos() {
+  return (
+    <div>
+      <h1>A Propos</h1>
+    </div>
+  )
+}
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Home() {
+export default function NotFound() {
   return (
     <div>
-      <h1>Acceuil</h1>
+      <h1>Erreur 404</h1>
     </div>
   )
 }
