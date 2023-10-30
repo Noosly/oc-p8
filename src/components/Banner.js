eaf17f242b4c158e3a59d657ea5c190666b0b03c
Noosly <139576679+Noosly@users.noreply.github.com>
2023-10-30 23:50:52 +0100

    Banner add SASS Style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import {Link, useLocation} from "react-router-dom"
 import logo from '../assets/LOGO.png'
+import '../styles/banner.css'
 
 export default function Banner() {
+  
   return (
     <div className="banner">
       <img className="banner__logo" src={logo} alt ="Logo Kasa"></img>
-      <Link to="/" className='banner__link'>Acceuil</Link>
-      <Link to="/Apropos" className='banner__link'>A Propos</Link>
+      <nav className='banner__navbar'>
+        <Link to="/" className='banner__link' >Acceuil</Link>
+        <Link to="/Apropos" className='banner__link' >A Propos</Link>
+      </nav>
+      
     </div>
   )
 }
