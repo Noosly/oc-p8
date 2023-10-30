ad7455c6a4124f12991b5b83ae4e368629d4415b
Noosly <139576679+Noosly@users.noreply.github.com>
2023-10-30 23:59:20 +0100

    Banner component useLocation to check isLinkActive to use banner__link--active class

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,16 @@ import logo from '../assets/LOGO.png'
 import '../styles/banner.css'
 
 export default function Banner() {
-  
+  const location = useLocation();
+  const isLinkActive = (to) => { 
+    return location.pathname === to;  
+  };
   return (
     <div className="banner">
       <img className="banner__logo" src={logo} alt ="Logo Kasa"></img>
       <nav className='banner__navbar'>
-        <Link to="/" className='banner__link' >Acceuil</Link>
-        <Link to="/Apropos" className='banner__link' >A Propos</Link>
+        <Link to="/" className={isLinkActive('/')?'banner__link--active':'banner__link'}>Acceuil</Link>
+        <Link to="/Apropos" className={isLinkActive('/Apropos')?'banner__link--active':'banner__link'}>A Propos</Link>
       </nav>
       
     </div>
