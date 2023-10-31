df21d34151289eafdf7a05c94ee25b4e5e22ed38
Noosly <139576679+Noosly@users.noreply.github.com>
2023-10-31 15:08:34 +0100

    Ajouter le components Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Card() {
-  return (
-    <div>
-      <h2>Card</h2>
-    </div>
+export default function Card({id, title, cover}) {
+  const  card_link = '/FicheLogement/' + {id}
+  return (    
+      <Link className='card' to={card_link}>       
+          <img className='card__cover' src={cover} alt='Aperçu du logement'/>
+          <h2 className='card__title'>{title}</h2>      
+      </Link>
+
+      
+
+    
   )
 }
