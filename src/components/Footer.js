914847a459a8dc994b4630dfcfd6be8c636026e4
Noosly <139576679+Noosly@users.noreply.github.com>
2023-11-01 16:15:51 +0100

    Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import logo from '../assets/LOGO_white.png'
+import '../styles/footer.css'
 
 export default function Footer() {
   return (
-    <div>
-      <h2>Footer</h2>
+    <div className='footer'>
+      <img className="footer__image" src={logo} alt ="Logo Kasa Footer"></img>
+      <div className='footer__text'>&copy;2020 Kasa. All rights reserved</div>
     </div>
   )
 }
