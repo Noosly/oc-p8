a0a366e6ccfd8ed77f41a2f1d8b13bb4b33dd546
Noosly <139576679+Noosly@users.noreply.github.com>
2023-11-09 23:49:28 +0100

    Slider: parcours des images avec les fleches gauches et droite

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,15 +3,29 @@ import arrow_left  from '../assets/arrow_left.png'
 import arrow_right from  '../assets/arrow_right.png'
 import '../styles/slider.css'
 import test1 from '../assets/bg_home.png'
+import { useState } from 'react'
 
 export default function Slider({pictures}) {
-  const picture = pictures[0] //'../assets/arrow_left.png'
-  
+  let [index, setIndex] = useState(0)
+  const nextSlide = (e) =>{ 
+    if(index == pictures.length - 1){
+      setIndex(0)
+    }else{
+      setIndex(index + 1)
+    }    
+  }
+  const previousSlide = (e) =>{
+    if(index == 0){
+      setIndex(pictures.length - 1)
+    }else{
+      setIndex(index - 1)
+    } 
+  }
   return (
     <div className='slider'>    
-        <img class="slider__cover" src={picture} alt="Photos du logement" />		
-        <img class="slider__arrow-left" src={arrow_left} alt="flèche gauche" />
-        <img class="slider__arrow-right" src={arrow_right} alt="flèche droite" />
+        <img className="slider__cover" src={pictures[index]} alt="Photos du logement" />		
+        <img className="slider__arrow-left" src={arrow_left} alt="flèche gauche" onClick={previousSlide} />
+        <img className="slider__arrow-right" src={arrow_right} alt="flèche droite" onClick={nextSlide} />
     </div>
   )
 }
