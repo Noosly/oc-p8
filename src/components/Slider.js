395bccf34a6517708c62d36c129f0ac3ab54af89
Noosly <139576679+Noosly@users.noreply.github.com>
2023-11-09 10:45:29 +0100

    Slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,12 +1,16 @@
 import React from 'react'
-
+import arrow_left  from '../assets/arrow_left.png'
+import arrow_right from  '../assets/arrow_right.png'
+import '../styles/slider.css'
+import test1 from '../assets/bg_home.png'
 export default function Slider() {
+  const url = '../assets/arrow_left.png';
+  const title = 'title';
   return (
-    <div className='slider-component'>
-
-      <h2>Slider</h2>
-      
-      
+    <div className='slider'>      
+        <img class="slider__cover" src={test1} alt="Photos du logement" />		
+        <img class="slider__arrow-left" src={arrow_left} alt="flèche gauche" />
+        <img class="slider__arrow-right" src={arrow_right} alt="flèche droite" />
     </div>
   )
 }
