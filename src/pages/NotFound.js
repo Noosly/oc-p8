c2bb1d96e87ba9468e0b21c698ee417e89928da8
Noosly <139576679+Noosly@users.noreply.github.com>
2023-11-15 00:49:09 +0100

    404: Page + Design

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import Footer from '../components/Footer'
+import Menu from '../components/Menu'
+import '../styles/not-found.css'
+import { Link } from 'react-router-dom'
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>Erreur 404</h1>
+    <div className='notfound-component'>
+      <header>
+        <Menu />
+      </header>
+      <main>
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to="/">Retourner sur la page d’accueil</Link>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
