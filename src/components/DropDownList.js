62ea468256ffb5c2b6136f76e5aab6a1e1b14633
Noosly <139576679+Noosly@users.noreply.github.com>
2023-11-26 14:48:22 +0100

    Amélioration Responsive Design

--- src/components/DropDownList.js
+++ src/components/DropDownList.js
@@ -13,7 +13,8 @@ export default function DropDownList({headerText, contentText}) {
         <div className='dropdownlist-header'>
             <div className='dropdownlist-header__text'>{headerText}</div>
             <div>
-              <img src={arrow} alt ="Flèche Haut/Bas" onClick={show_hide} className={open ? 'dropdownlist-header__image--closed' : 'dropdownlist-header__image--opened'}/>
+              <img src={arrow} alt ="Flèche Haut/Bas" onClick={show_hide} 
+                         className={open ? 'dropdownlist-header__image--opened' : 'dropdownlist-header__image--closed'}/>
             </div>
         </div> 
         <div className='dropdownlist-content'>
